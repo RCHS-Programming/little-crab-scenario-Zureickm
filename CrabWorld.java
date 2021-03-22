7cb6171c4090f64485f2f55e031dd6d30f3c6c65
Zureick <zureickm@gmail.com>
2021-03-22 08:41:11 -0400

    Errors

--- CrabWorld.java
+++ CrabWorld.java
@@ -9,5 +9,32 @@ public class CrabWorld extends World
     public CrabWorld() 
     {
         super(560, 560, 1);
+        addObject (new Crab(),150,100);
+        addObject (new Lobster(),400,100);
+        addObject (new Lobster(),400,300);
+        addObject (new Lobster(),400,500);
+        addObject (new Worm(), 300, 50);
+        addObject (new Worm(), 300, 100);
+        addObject (new Worm(), 300, 150);
+        addObject (new Worm(), 300, 200);
+        addObject (new Worm(), 300, 250);
+        addObject (new Worm(), 300, 300);
+        addObject (new Worm(), 300, 350);
+        addObject (new Worm(), 300, 400);
+        addObject (new Worm(), 300, 450);
+        addObject (new Worm(), 300, 500);
     }
+}
+//
+public class Crab extends Animal
+{
+    private GreenfootImage image1;
+    private GreenfootImage image2;
+}
+ 
+public Crab()
+{
+    image1 = new GreenfootImage ("Crab.png");
+    image2 = new GreenfootImage ("Crab2.png");
+    setImage(image1);
 }
